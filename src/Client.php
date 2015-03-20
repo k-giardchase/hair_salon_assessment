cb01145dd4e5d9a2d69191105ee5b7b86d6dabde
Kyle <kgiardch@gmail.com>
2015-03-20 14:23:54 -0700

    Add getAll method to Client.php

--- src/Client.php
+++ src/Client.php
@@ -5,7 +5,7 @@
         private $client_name;
         private $id;
 
-        function __construct($client_name, $id)
+        function __construct($client_name, $id = null)
         {
             $this->client_name = $client_name;
             $this->id = $id;
@@ -33,7 +33,22 @@
 
         function save()
         {
-            $GLOBALS['DB']->query("INSERT INTO clients (client_name) VALUES '{$this->getClientName()}';");
+            $statement = $GLOBALS['DB']->query("INSERT INTO clients (client_name) VALUES ('{$this->getClientName()}') RETURNING id;");
+            $result = $statement->fetch(PDO::FETCH_ASSOC);
+            $this->setId($result['id']);
+        }
+
+        static function getAll()
+        {
+            $all_clients = array();
+            $returned_clients = $GLOBALS['DB']->query("SELECT * FROM clients;");
+            foreach($returned_clients as $client){
+                $client_name = $client['client_name'];
+                $id = $client['id'];
+                $new_client = new Client($client_name, $id);
+                array_push($all_clients, $new_client);
+            }
+            return $all_clients;
         }
     }
 
