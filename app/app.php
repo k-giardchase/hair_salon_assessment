7ee8a9af324e0cdb5d2547e2a61c76f20607c831
Kyle <kgiardch@gmail.com>
2015-03-20 11:36:14 -0700

    Add app.php file to app folder. Create first route to home page.

---
+++ app/app.php
@@ -0,0 +1,23 @@
+<?php
+
+    require_once __DIR__."/../vendor/autoload.php";
+    require_once __DIR__."/../src/Stylist.php";
+
+    $app = new Silex\Application();
+
+    $DB = new PDO('pgsql:host=localhost;dbname=hair_salon');
+
+    $app->register(new Silex\Provider\TwigServiceProvider(), array(
+        'twig.path' => __DIR__.'/../views'
+    ));
+
+    $app->get('/', function() use ($app){
+
+        return $app['twig']->render('stylists.twig');
+    });
+
+
+
+
+    return $app;
+?>
