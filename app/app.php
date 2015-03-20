dd377da0d618282af55f967b06542d34ee0a1a20
Kyle <kgiardch@gmail.com>
2015-03-20 12:19:54 -0700

    Add route for single stylist display

--- app/app.php
+++ app/app.php
@@ -11,22 +11,42 @@
         'twig.path' => __DIR__.'/../views'
     ));
 
-    $app->get('/', function() use ($app) {
+    //Render home page
+    $app->get('/stylists', function() use ($app) {
 
         return $app['twig']->render('stylists.html.twig');
     });
 
-    $app->post('/create_stylist', function() use ($app) {
+    //List and post stylists to home page
+    $app->post('/stylists', function() use ($app) {
         $stylist_name = $_POST['stylist_name'];
         $new_stylist = new Stylist($stylist_name, $id = null);
         $new_stylist->save();
         return $app['twig']->render('stylists.html.twig', array('stylists' => Stylist::getAll()));
     });
 
+    //Delete all stylists from home page
     $app->post('/delete_stylists', function() use ($app) {
         Stylist::deleteAll();
         return $app['twig']->render('stylists.html.twig', array('stylists' => Stylist::getAll()));
     });
+
+    //Render single stylist page
+    $app->get('/stylist{id}', function($id) use ($app) {
+        $selected_stylist = Stylist::find($id)
+        return $app['twig']->render('stylist.html.twig', array('stylist' => $selected_stylist);
+    });
+
+    //Delete a single stylist from stylist page
+    $app->post('/delete_stylist', function() use ($app) {
+
+    })
+
+
+
+
+
+
 
 
 
