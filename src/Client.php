81a0033a07f942af09aa362b0412d5af9e2f0c63
Kyle <kgiardch@gmail.com>
2015-03-20 14:37:18 -0700

    Add find method to Client.php to pass testFind

--- src/Client.php
+++ src/Client.php
@@ -38,6 +38,19 @@
             $this->setId($result['id']);
         }
 
+        static function find($search_id)
+        {
+            $found_client = null;
+            $clients = Client::getAll();
+            foreach($clients as $client){
+                $client_id = $client->getId();
+                if($client_id == $search_id) {
+                    $found_client = $client;
+                }
+            }
+            return $found_client;
+        }
+
 
         static function getAll()
         {
