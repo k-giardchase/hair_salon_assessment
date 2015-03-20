38b7e81d65de3e514292b27498c68c1a01061404
Kyle <kgiardch@gmail.com>
2015-03-20 11:59:35 -0700

    Add form to stylists.html.twig to delete all stylists. Create route for form.

--- app/app.php
+++ app/app.php
@@ -16,6 +16,17 @@
         return $app['twig']->render('stylists.html.twig');
     });
 
+    $app->post('/create_stylist', function() use ($app) {
+        $stylist_name = $_POST['stylist_name'];
+        $new_stylist = new Stylist($stylist_name, $id = null);
+        $new_stylist->save();
+        return $app['twig']->render('stylists.html.twig', array('stylists' => Stylist::getAll()));
+    });
+
+    $app->post('/delete_stylists', function() use ($app) {
+        Stylist::deleteAll();
+        return $app['twig']->render('stylists.html.twig', array('stylists' => Stylist::getAll()));
+    });
 
 
 
