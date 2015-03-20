3912d234af016c481120ea6d55963ec3688b5cdd
Kyle <kgiardch@gmail.com>
2015-03-20 10:01:27 -0700

    Add functionality for getAll method to Stylist.php

--- src/Stylist.php
+++ src/Stylist.php
@@ -24,6 +24,18 @@
             $GLOBALS['DB']->exec("INSERT INTO stylists (stylist_name) VALUES ('{$this->getStylist()}'
         );");
         }
+
+        function getAll()
+        {
+            $all_stylists = array();
+            $returned_stylists = $GLOBALS['DB']->query("SELECT * FROM stylists;");
+            foreach($returned_stylists as $returned_stylist){
+                $stylist = $returned_stylist['stylist'];
+                $new_stylist = new Stylist($stylist);
+                array_push($all_stylists, $new_stylist);
+            }
+            return $all_stylists;
+        }
     }
 
 ?>
